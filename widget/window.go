88e2426ed3e96857d3c98c4bf4f43a676a9d0027
Mark Carpenter <mark@dmn-dev.com>
2023-01-17 17:47:02 -0500

    Add Resize/Drag to window object

--- widget/window.go
+++ widget/window.go
@@ -3,17 +3,44 @@ package widget
 import (
 	"image"
 
+	"github.com/ebitenui/ebitenui/event"
 	"github.com/ebitenui/ebitenui/input"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type RemoveWindowFunc func()
 
+type WindowChangedEventArgs struct {
+	Window *Window
+	Rect   image.Rectangle
+}
+
+type WindowChangedHandlerFunc func(args *WindowChangedEventArgs)
+
 type Window struct {
-	Modal        bool
+	ResizeEvent *event.Event
+	MoveEvent   *event.Event
+
+	Modal      bool
+	Contents   *Container
+	TitleBar   *Container
+	Draggable  bool
+	Resizeable bool
+	MinSize    *image.Point
+	MaxSize    *image.Point
+
 	closeOnClick bool
 	closeFunc    RemoveWindowFunc
-	Contents     *Container
+	container    *Container
+
+	titleBarHeight int
+
+	startingPoint  image.Point
+	dragging       bool
+	resizing       bool
+	resizingWidth  bool
+	resizingHeight bool
+	originalSize   image.Point
 }
 
 type WindowOpt func(w *Window)
@@ -24,13 +51,69 @@ type WindowOptions struct {
 var WindowOpts WindowOptions
 
 func NewWindow(opts ...WindowOpt) *Window {
-	w := &Window{}
-
+	w := &Window{
+		MoveEvent:   &event.Event{},
+		ResizeEvent: &event.Event{},
+	}
 	for _, o := range opts {
 		o(w)
 	}
+
+	if w.TitleBar != nil {
+		w.container = NewContainer(ContainerOpts.Layout(NewGridLayout(
+			GridLayoutOpts.Columns(1),
+			GridLayoutOpts.Stretch([]bool{true}, []bool{false, true}),
+		)))
+		w.TitleBar.GetWidget().LayoutData = GridLayoutData{MaxHeight: w.titleBarHeight}
+		w.TitleBar.GetWidget().MinHeight = w.titleBarHeight
+		if w.Draggable {
+			w.TitleBar.GetWidget().MouseButtonPressedEvent.AddHandler(func(_ interface{}) {
+				x, y := input.CursorPosition()
+				w.startingPoint = image.Point{x, y}
+				w.dragging = true
+			})
+			w.TitleBar.GetWidget().MouseButtonReleasedEvent.AddHandler(func(_ interface{}) {
+				if w.dragging {
+					w.dragging = false
+					w.MoveEvent.Fire(&WindowChangedEventArgs{
+						Window: w,
+						Rect:   w.container.GetWidget().Rect,
+					})
+				}
+			})
+		}
+		w.container.AddChild(w.TitleBar)
+		w.container.AddChild(w.Contents)
+	} else {
+		w.container = NewContainer(ContainerOpts.Layout(NewGridLayout(
+			GridLayoutOpts.Columns(1),
+			GridLayoutOpts.Stretch([]bool{true}, []bool{true}),
+		)))
+
+		w.container.AddChild(w.Contents)
+	}
+
+	if w.Resizeable {
+		w.Contents.GetWidget().MouseButtonPressedEvent.AddHandler(func(_ interface{}) {
+			x, y := input.CursorPosition()
+			w.startingPoint = image.Point{x, y}
+			w.originalSize.X = w.container.GetWidget().Rect.Max.X
+			w.originalSize.Y = w.container.GetWidget().Rect.Max.Y
+			w.resizing = true
+		})
+		w.Contents.GetWidget().MouseButtonReleasedEvent.AddHandler(func(_ interface{}) {
+			if w.resizing {
+				w.resizing = false
+				w.ResizeEvent.Fire(&WindowChangedEventArgs{
+					Window: w,
+					Rect:   w.container.GetWidget().Rect,
+				})
+			}
+		})
+	}
+
 	if w.closeOnClick {
-		w.Contents.GetWidget().MouseButtonReleasedEvent.AddHandler(func(args interface{}) {
+		w.container.GetWidget().MouseButtonReleasedEvent.AddHandler(func(args interface{}) {
 			a := args.(*WidgetMouseButtonReleasedEventArgs)
 			if !a.Inside && w.closeFunc != nil {
 				w.closeFunc()
@@ -46,15 +129,62 @@ func (o WindowOptions) Contents(c *Container) WindowOpt {
 	}
 }
 
+func (o WindowOptions) TitleBar(tb *Container, height int) WindowOpt {
+	return func(w *Window) {
+		w.TitleBar = tb
+		w.titleBarHeight = height
+	}
+}
+
 func (o WindowOptions) Modal() WindowOpt {
 	return func(w *Window) {
 		w.Modal = true
 	}
 }
 
+func (o WindowOptions) Draggable() WindowOpt {
+	return func(w *Window) {
+		w.Draggable = true
+	}
+}
+
+func (o WindowOptions) Resizeable() WindowOpt {
+	return func(w *Window) {
+		w.Resizeable = true
+	}
+}
+
+func (o WindowOptions) MinSize(width int, height int) WindowOpt {
+	return func(w *Window) {
+		w.MinSize = &image.Point{X: width, Y: height}
+	}
+}
+
+func (o WindowOptions) MaxSize(width int, height int) WindowOpt {
+	return func(w *Window) {
+		w.MaxSize = &image.Point{X: width, Y: height}
+	}
+}
+
 func (o WindowOptions) CloseOnClickOut() WindowOpt {
 	return func(w *Window) {
 		w.closeOnClick = true
+	}
+}
+
+func (o WindowOptions) MoveHandler(f WindowChangedHandlerFunc) WindowOpt {
+	return func(w *Window) {
+		w.MoveEvent.AddHandler(func(args interface{}) {
+			f(args.(*WindowChangedEventArgs))
+		})
+	}
+}
+
+func (o WindowOptions) ResizeHandler(f WindowChangedHandlerFunc) WindowOpt {
+	return func(w *Window) {
+		w.ResizeEvent.AddHandler(func(args interface{}) {
+			f(args.(*WindowChangedEventArgs))
+		})
 	}
 }
 
@@ -69,16 +199,34 @@ func (w *Window) Close() {
 }
 
 func (w *Window) SetLocation(rect image.Rectangle) {
-	w.Contents.SetLocation(rect)
+	if w.MinSize != nil {
+		if rect.Dx() < w.MinSize.X {
+			rect.Max.X = rect.Min.X + w.MinSize.X
+		}
+		if rect.Dy() < w.MinSize.Y {
+			rect.Max.Y = rect.Min.Y + w.MinSize.Y
+		}
+	}
+
+	if w.MaxSize != nil {
+		if rect.Dx() > w.MaxSize.X {
+			rect.Max.X = rect.Min.X + w.MaxSize.X
+		}
+		if rect.Dy() > w.MaxSize.Y {
+			rect.Max.Y = rect.Min.Y + w.MaxSize.Y
+		}
+	}
+
+	w.container.SetLocation(rect)
 }
 
 func (w *Window) RequestRelayout() {
-	w.Contents.RequestRelayout()
+	w.container.RequestRelayout()
 }
 
 func (w *Window) SetupInputLayer(def input.DeferredSetupInputLayerFunc) {
 	if w.Modal {
-		w.Contents.GetWidget().ElevateToNewInputLayer(&input.Layer{
+		w.container.GetWidget().ElevateToNewInputLayer(&input.Layer{
 			DebugLabel: "modal window",
 			EventTypes: input.LayerEventTypeAll,
 			BlockLower: true,
@@ -88,5 +236,52 @@ func (w *Window) SetupInputLayer(def input.DeferredSetupInputLayerFunc) {
 }
 
 func (w *Window) Render(screen *ebiten.Image, def DeferredRenderFunc) {
-	w.Contents.Render(screen, def)
+	x, y := input.CursorPosition()
+
+	if w.dragging {
+		if w.startingPoint.X != x || w.startingPoint.Y != y {
+			newRect := w.container.GetWidget().Rect.Add(image.Point{x - w.startingPoint.X, y - w.startingPoint.Y})
+			w.SetLocation(newRect)
+			w.RequestRelayout()
+			w.startingPoint = image.Point{x, y}
+		}
+	}
+	if w.resizing {
+		if w.startingPoint.X != x || w.startingPoint.Y != y {
+			if w.resizingWidth {
+				newRect := w.container.GetWidget().Rect
+				newRect.Max.X = w.originalSize.X - (w.startingPoint.X - x)
+				w.SetLocation(newRect)
+				w.RequestRelayout()
+			}
+			if w.resizingHeight {
+				newRect := w.container.GetWidget().Rect
+				newRect.Max.Y = w.originalSize.Y - (w.startingPoint.Y - y)
+
+				w.SetLocation(newRect)
+				w.RequestRelayout()
+			}
+		}
+	}
+	if w.Resizeable {
+		if w.container.GetWidget().inputLayer.ActiveFor(x, y, input.LayerEventTypeAll) {
+			xRect := image.Rect(w.container.GetWidget().Rect.Max.X-4, w.container.GetWidget().Rect.Min.Y, w.container.GetWidget().Rect.Max.X, w.container.GetWidget().Rect.Max.Y)
+			yRect := image.Rect(w.container.GetWidget().Rect.Min.X, w.container.GetWidget().Rect.Max.Y-4, w.container.GetWidget().Rect.Max.X, w.container.GetWidget().Rect.Max.Y)
+			cursorRect := image.Rect(x, y, x+1, y+1)
+			if cursorRect.Overlaps(xRect) {
+				ebiten.SetCursorShape(ebiten.CursorShapeEWResize)
+				w.resizingWidth = true
+				w.resizingHeight = false
+			} else if cursorRect.Overlaps(yRect) {
+				ebiten.SetCursorShape(ebiten.CursorShapeNSResize)
+				w.resizingWidth = false
+				w.resizingHeight = true
+			} else {
+				ebiten.SetCursorShape(ebiten.CursorShapeDefault)
+				w.resizingWidth = false
+				w.resizingHeight = false
+			}
+		}
+	}
+	w.container.Render(screen, def)
 }
