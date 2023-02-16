b6e2bcbecae105853b6ae44d60743e2fad7f18c3
Mark Carpenter <mark@dmn-dev.com>
2023-02-16 17:00:01 -0500

    Add textinput demo

---
+++ _examples/widget_demos/textinput/main.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"log"
+	"strings"
+
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
+	"github.com/golang/freetype/truetype"
+	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
+)
+
+// Game object used by ebiten
+type game struct {
+	ui *ebitenui.UI
+}
+
+func main() {
+
+	// load button text font
+	face, _ := loadFont(20)
+
+	// construct a new container that serves as the root of the UI hierarchy
+	rootContainer := widget.NewContainer(
+		// the container will use a plain color as its background
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.RGBA{0x13, 0x1a, 0x22, 0xff})),
+
+		// the container will use a row layout to layout the textinput widgets
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+			widget.RowLayoutOpts.Spacing(20),
+			widget.RowLayoutOpts.Padding(widget.NewInsetsSimple(20)))),
+	)
+
+	// construct a standard textinput widget
+	standardTextInput := widget.NewTextInput(
+		widget.TextInputOpts.WidgetOpts(
+			//Set the layout information to center the textbox in the parent
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionCenter,
+				Stretch:  true,
+			}),
+		),
+
+		//Set the Idle and Disabled background image for the text input
+		//If the NineSlice image has a minimum size, the widget will use that or
+		// widget.WidgetOpts.MinSize; whichever is greater
+		widget.TextInputOpts.Image(&widget.TextInputImage{
+			Idle:     image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+			Disabled: image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+		}),
+
+		//Set the font face and size for the widget
+		widget.TextInputOpts.Face(face),
+
+		//Set the colors for the text and caret
+		widget.TextInputOpts.Color(&widget.TextInputColor{
+			Idle:          color.White,
+			Disabled:      color.RGBA{R: 200, G: 200, B: 200, A: 255},
+			Caret:         color.White,
+			DisabledCaret: color.RGBA{R: 200, G: 200, B: 200, A: 255},
+		}),
+
+		//Set how much padding there is between the edge of the input and the text
+		widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
+
+		//Set the font and width of the caret
+		widget.TextInputOpts.CaretOpts(
+			widget.CaretOpts.Size(face, 2),
+		),
+
+		//This text is displayed if the input is empty
+		widget.TextInputOpts.Placeholder("Standard Textbox"),
+
+		//This is called when the user hits the "Enter" key.
+		//There are other options that can configure this behavior
+		widget.TextInputOpts.SubmitHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Submitted: ", args.InputText)
+		}),
+
+		//This is called whenver there is a change to the text
+		widget.TextInputOpts.ChangedHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Changed: ", args.InputText)
+		}),
+	)
+
+	rootContainer.AddChild(standardTextInput)
+
+	// construct a secure textinput widget
+	secureTextInput := widget.NewTextInput(
+		widget.TextInputOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionCenter,
+				Stretch:  true,
+			}),
+		),
+		widget.TextInputOpts.Image(&widget.TextInputImage{
+			Idle:     image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+			Disabled: image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+		}),
+		widget.TextInputOpts.Face(face),
+		widget.TextInputOpts.Color(&widget.TextInputColor{
+			Idle:          color.White,
+			Disabled:      color.RGBA{R: 200, G: 200, B: 200, A: 255},
+			Caret:         color.White,
+			DisabledCaret: color.RGBA{R: 200, G: 200, B: 200, A: 255},
+		}),
+		widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
+		widget.TextInputOpts.CaretOpts(
+			widget.CaretOpts.Size(face, 2),
+		),
+		//This parameter indicates that the inputted text should be hidden
+		widget.TextInputOpts.Secure(true),
+
+		widget.TextInputOpts.Placeholder("Password Textbox"),
+		widget.TextInputOpts.SubmitHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Submitted: ", args.InputText)
+		}),
+		widget.TextInputOpts.ChangedHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Changed: ", args.InputText)
+		}),
+	)
+
+	rootContainer.AddChild(secureTextInput)
+
+	maxLenTextInput := widget.NewTextInput(
+		widget.TextInputOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionCenter,
+				Stretch:  true,
+			}),
+		),
+		widget.TextInputOpts.Image(&widget.TextInputImage{
+			Idle:     image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+			Disabled: image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+		}),
+		widget.TextInputOpts.Face(face),
+		widget.TextInputOpts.Color(&widget.TextInputColor{
+			Idle:          color.White,
+			Disabled:      color.RGBA{R: 200, G: 200, B: 200, A: 255},
+			Caret:         color.White,
+			DisabledCaret: color.RGBA{R: 200, G: 200, B: 200, A: 255},
+		}),
+		widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
+		widget.TextInputOpts.CaretOpts(
+			widget.CaretOpts.Size(face, 2),
+		),
+		widget.TextInputOpts.Placeholder("Max length (5) Textbox"),
+
+		//This method is called whenever there is a text change.
+		//It allows the developer to allow or deny a change.
+		//In this case we are limiting the string to 5 characters.
+		//The first return parameter is whether or not to accept the text as is.
+		//The second return parameter is what to replace the text with if it is not accepted (optional)
+		widget.TextInputOpts.Validation(func(newInputText string) (bool, *string) {
+			if len(newInputText) > 5 {
+				return false, nil
+			}
+			return true, nil
+		}),
+		widget.TextInputOpts.SubmitHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Submitted: ", args.InputText)
+		}),
+		widget.TextInputOpts.ChangedHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Changed: ", args.InputText)
+		}),
+	)
+
+	rootContainer.AddChild(maxLenTextInput)
+
+	allCapsTextInput := widget.NewTextInput(
+		widget.TextInputOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionCenter,
+				Stretch:  true,
+			}),
+		),
+		widget.TextInputOpts.Image(&widget.TextInputImage{
+			Idle:     image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+			Disabled: image.NewNineSliceColor(color.RGBA{R: 100, G: 100, B: 100, A: 255}),
+		}),
+		widget.TextInputOpts.Face(face),
+		widget.TextInputOpts.Color(&widget.TextInputColor{
+			Idle:          color.White,
+			Disabled:      color.RGBA{R: 200, G: 200, B: 200, A: 255},
+			Caret:         color.White,
+			DisabledCaret: color.RGBA{R: 200, G: 200, B: 200, A: 255},
+		}),
+		widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
+		widget.TextInputOpts.CaretOpts(
+			widget.CaretOpts.Size(face, 2),
+		),
+		widget.TextInputOpts.Placeholder("All Caps Textbox"),
+
+		//This method is called whenever there is a text change.
+		//It allows the developer to allow or deny a change.
+		//In this case we are forcing the string to be all caps.
+		//The first return parameter is whether or not to accept the text as is.
+		//The second return parameter is what to replace the text with if it is not accepted (optional)
+		widget.TextInputOpts.Validation(func(newInputText string) (bool, *string) {
+			newInputText = strings.ToUpper(newInputText)
+			return false, &newInputText
+		}),
+		widget.TextInputOpts.SubmitHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Submitted: ", args.InputText)
+		}),
+		widget.TextInputOpts.ChangedHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Changed: ", args.InputText)
+		}),
+	)
+
+	rootContainer.AddChild(allCapsTextInput)
+	// construct the UI
+	ui := ebitenui.UI{
+		Container: rootContainer,
+	}
+
+	// Ebiten setup
+	ebiten.SetWindowSize(400, 400)
+	ebiten.SetWindowTitle("Ebiten UI - TextInput")
+
+	game := game{
+		ui: &ui,
+	}
+
+	// run Ebiten main loop
+	err := ebiten.RunGame(&game)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// Layout implements Game.
+func (g *game) Layout(outsideWidth int, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
+}
+
+// Update implements Game.
+func (g *game) Update() error {
+	// update the UI
+	g.ui.Update()
+	return nil
+}
+
+// Draw implements Ebiten's Draw method.
+func (g *game) Draw(screen *ebiten.Image) {
+	// draw the UI onto the screen
+	g.ui.Draw(screen)
+}
+
+func loadFont(size float64) (font.Face, error) {
+	ttfFont, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		return nil, err
+	}
+
+	return truetype.NewFace(ttfFont, &truetype.Options{
+		Size:    size,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	}), nil
+}
