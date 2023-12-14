6c058c2b41f916ade735093b7d7ace7677f60c40
Mathieu Champlon <mathieu.champlon@docker.com>
2023-12-14 07:23:29 +0100

    Remove drag&drop extra update call
    There is already an update triggered right after, with the correct data e.g. the hovered element. This extra update was always done with droppable false and no element.

--- widget/dnd.go
+++ widget/dnd.go
@@ -228,7 +228,6 @@ func (d *DragAndDrop) draggingState(srcX int, srcY int, dragWidget *Container, d
 			droppable := false
 			var element HasWidget
 
-			u.Update(droppable, element, dragData)
 			args := &DragAndDropDroppedEventArgs{
 				Source:  parent,
 				SourceX: srcX,
