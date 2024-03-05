cc65b57db5f258a6e054719386b85392903244e4
Mark Carpenter <mark@dmn-dev.com>
2024-03-04 21:24:30 -0500

    Revert changes to button example

--- _examples/widget_demos/button/main.go
+++ _examples/widget_demos/button/main.go
@@ -15,8 +15,7 @@ import (
 
 // Game object used by ebiten
 type game struct {
-	ui      *ebitenui.UI
-	button2 *widget.Button
+	ui *ebitenui.UI
 }
 
 func main() {
@@ -32,7 +31,7 @@ func main() {
 		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{0x13, 0x1a, 0x22, 0xff})),
 
 		// the container will use an anchor layout to layout its single child widget
-		widget.ContainerOpts.Layout(widget.NewRowLayout()),
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
 	)
 
 	// construct a button
@@ -40,8 +39,9 @@ func main() {
 		// set general widget options
 		widget.ButtonOpts.WidgetOpts(
 			// instruct the container's anchor layout to center the button both horizontally and vertically
-			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
-				Position: widget.RowLayoutPositionCenter,
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionCenter,
 			}),
 		),
 
@@ -70,42 +70,6 @@ func main() {
 	// add the button as a child of the container
 	rootContainer.AddChild(button)
 
-	// construct a button
-	button2 := widget.NewButton(
-		// set general widget options
-		widget.ButtonOpts.WidgetOpts(
-			// instruct the container's anchor layout to center the button both horizontally and vertically
-			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
-				Position: widget.RowLayoutPositionCenter,
-			}),
-			widget.WidgetOpts.ToolTip(widget.NewTextToolTip("Test", face, color.Black, nil)),
-		),
-
-		// specify the images to use
-		widget.ButtonOpts.Image(buttonImage),
-
-		// specify the button's text, the font face, and the color
-		widget.ButtonOpts.Text("Button 2", face, &widget.ButtonTextColor{
-			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
-		}),
-
-		// specify that the button's text needs some padding for correct display
-		widget.ButtonOpts.TextPadding(widget.Insets{
-			Left:   30,
-			Right:  30,
-			Top:    5,
-			Bottom: 5,
-		}),
-
-		// add a handler that reacts to clicking the button
-		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
-			println("button clicked")
-		}),
-	)
-
-	// add the button as a child of the container
-	rootContainer.AddChild(button2)
-
 	// construct the UI
 	ui := ebitenui.UI{
 		Container: rootContainer,
@@ -116,8 +80,7 @@ func main() {
 	ebiten.SetWindowTitle("Ebiten UI - Buttons")
 
 	game := game{
-		ui:      &ui,
-		button2: button2,
+		ui: &ui,
 	}
 
 	// run Ebiten main loop
@@ -135,9 +98,6 @@ func (g *game) Layout(outsideWidth int, outsideHeight int) (int, int) {
 // Update implements Game.
 func (g *game) Update() error {
 	// update the UI
-	if ebiten.IsKeyPressed(ebiten.KeyEnter) {
-		g.ui.Container.RemoveChild(g.button2)
-	}
 	g.ui.Update()
 	return nil
 }
