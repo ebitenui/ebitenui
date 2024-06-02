9536f51743a8641c7d16aa63e1b0a180954c43bb
scratch85 <430899+scratch85@users.noreply.github.com>
2024-06-02 18:10:12 +0200

    add example to use button with mask

---
+++ _examples/widget_demos/button_mask/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
+	"github.com/golang/freetype/truetype"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
+)
+
+// Game object used by ebiten
+type game struct {
+	ui  *ebitenui.UI
+	btn *widget.Button
+}
+
+func main() {
+	// load images for button states: idle, hover, and pressed
+	buttonImage, _ := loadButtonImage()
+
+	// load button text font
+	face, _ := loadFont(20)
+
+	// construct a new container that serves as the root of the UI hierarchy
+	rootContainer := widget.NewContainer(
+		// the container will use a plain color as its background
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{0x13, 0x1a, 0x22, 0xff})),
+
+		// the container will use an anchor layout to layout its single child widget
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+
+	// construct a button
+	button := widget.NewButton(
+		// set general widget options
+		widget.ButtonOpts.WidgetOpts(
+			// instruct the container's anchor layout to center the button both horizontally and vertically
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionCenter,
+			}),
+			widget.WidgetOpts.MinSize(100, 100),
+		),
+
+		// specify the images to use
+		widget.ButtonOpts.Image(buttonImage),
+
+		// specify the button's text, the font face, and the color
+		//widget.ButtonOpts.Text("Hello, World!", face, &widget.ButtonTextColor{
+		//widget.ButtonOpts.Text("Hello, [color=FF00FF]World![/color]", face, &widget.ButtonTextColor{
+		widget.ButtonOpts.Text("Hello!", face, &widget.ButtonTextColor{
+			Idle:  color.NRGBA{255, 255, 255, 255},
+			Hover: color.NRGBA{0, 0, 0, 255},
+		}),
+		//widget.ButtonOpts.TextProcessBBCode(true),
+		// specify that the button's text needs some padding for correct display
+		/*
+			widget.ButtonOpts.TextPadding(widget.Insets{
+				Left:   30,
+				Right:  30,
+				Top:    5,
+				Bottom: 5,
+			}),
+		*/
+
+		// add a handler that reacts to clicking the button
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			println("button clicked")
+		}),
+
+		// Indicate that this button should not be submitted when enter or space are pressed
+		// widget.ButtonOpts.DisableDefaultKeys(),
+	)
+
+	// add the button as a child of the container
+	rootContainer.AddChild(button)
+
+	// construct the UI
+	ui := ebitenui.UI{
+		Container: rootContainer,
+	}
+
+	// Ebiten setup
+	ebiten.SetWindowSize(400, 400)
+	ebiten.SetWindowTitle("Ebiten UI - Buttons with mask")
+
+	game := game{
+		ui:  &ui,
+		btn: button,
+	}
+
+	// run Ebiten main loop
+	err := ebiten.RunGame(&game)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// Layout implements Game.
+func (g *game) Layout(outsideWidth int, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
+}
+
+// Update implements Game.
+func (g *game) Update() error {
+	// update the UI
+	g.ui.Update()
+	if inpututil.IsKeyJustPressed(ebiten.KeyB) {
+		g.btn.Click()
+	}
+
+	//Test that you can call Click on the focused widget.
+	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
+		if btn, ok := g.ui.GetFocusedWidget().(*widget.Button); ok {
+			btn.Click()
+		}
+	}
+
+	return nil
+}
+
+// Draw implements Ebiten's Draw method.
+func (g *game) Draw(screen *ebiten.Image) {
+	// draw the UI onto the screen
+	g.ui.Draw(screen)
+}
+
+func loadButtonImage() (*widget.ButtonImage, error) {
+	idleImage := ebiten.NewImage(100, 100)
+	idleImage.Fill(color.RGBA{R: 80, G: 80, B: 80, A: 255})
+	vector.DrawFilledCircle(idleImage, float32(idleImage.Bounds().Dx())/2, float32(idleImage.Bounds().Dy())/2, 45, color.Black, true)
+	vector.DrawFilledCircle(idleImage, float32(idleImage.Bounds().Dx())/2, float32(idleImage.Bounds().Dy())/2, 20, color.RGBA{R: 80, G: 80, B: 80, A: 255}, true)
+	idle := image.NewNineSlice(idleImage, [3]int{0, idleImage.Bounds().Dx(), 0}, [3]int{0, idleImage.Bounds().Dy(), 0})
+
+	hoverImage := ebiten.NewImage(100, 100)
+	hoverImage.Fill(color.RGBA{R: 80, G: 80, B: 80, A: 255})
+	vector.DrawFilledCircle(hoverImage, float32(hoverImage.Bounds().Dx())/2, float32(hoverImage.Bounds().Dy())/2, 45, color.White, true)
+	vector.DrawFilledCircle(hoverImage, float32(hoverImage.Bounds().Dx())/2, float32(hoverImage.Bounds().Dy())/2, 20, color.RGBA{R: 80, G: 80, B: 80, A: 255}, true)
+	hover := image.NewNineSlice(hoverImage, [3]int{0, hoverImage.Bounds().Dx(), 0}, [3]int{0, hoverImage.Bounds().Dy(), 0})
+
+	pressedImage := ebiten.NewImage(100, 100)
+	pressedImage.Fill(color.RGBA{R: 80, G: 80, B: 80, A: 255})
+	vector.DrawFilledCircle(pressedImage, float32(pressedImage.Bounds().Dx())/2, float32(pressedImage.Bounds().Dy())/2, 45, color.RGBA{R: 255, G: 0, B: 0, A: 255}, true)
+	vector.DrawFilledCircle(pressedImage, float32(pressedImage.Bounds().Dx())/2, float32(pressedImage.Bounds().Dy())/2, 20, color.RGBA{R: 80, G: 80, B: 80, A: 255}, true)
+	pressed := image.NewNineSlice(pressedImage, [3]int{0, pressedImage.Bounds().Dx(), 0}, [3]int{0, pressedImage.Bounds().Dy(), 0})
+
+	maskImage := ebiten.NewImage(100, 100)
+	maskImage.Fill(color.RGBA{R: 0, G: 0, B: 0, A: 255})
+	vector.DrawFilledCircle(maskImage, float32(maskImage.Bounds().Dx())/2, float32(maskImage.Bounds().Dy())/2, 45, color.RGBA{R: 255, G: 255, B: 255, A: 255}, true)
+	vector.DrawFilledCircle(maskImage, float32(maskImage.Bounds().Dx())/2, float32(maskImage.Bounds().Dy())/2, 20, color.RGBA{R: 0, G: 0, B: 0, A: 255}, true)
+	mask := image.NewNineSlice(maskImage, [3]int{0, maskImage.Bounds().Dx(), 0}, [3]int{0, maskImage.Bounds().Dy(), 0})
+
+	return &widget.ButtonImage{
+		Idle:    idle,
+		Hover:   hover,
+		Pressed: pressed,
+		Mask:    mask,
+	}, nil
+}
+
+func loadFont(size float64) (font.Face, error) {
+	ttfFont, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		return nil, err
+	}
+
+	return truetype.NewFace(ttfFont, &truetype.Options{
+		Size:    size,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	}), nil
+}
