a768854653f8aa8ebbc817dff2e447db9382a8b4
potpiefox <178157119+potpiefox@users.noreply.github.com>
2024-08-28 19:15:06 -0500

    Add toolbar example

---
+++ _examples/toolbar/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bytes"
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/widget"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/gofont/goregular"
+	"log"
+)
+
+const (
+	Width  = 800
+	Height = 600
+)
+
+func main() {
+	// Set up Ebiten
+	ebiten.SetWindowSize(Width, Height)
+	ebiten.SetWindowTitle("Ebitenui Toolbar Demo")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	res, err := loadResources()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Construct a new container that serves as the root of the UI hierarchy.
+	root := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+
+	ui := ebitenui.UI{
+		Container: root,
+	}
+
+	// Create a toolbar and add it to the UI.
+	toolbar := newToolbar(&ui, res)
+	root.AddChild(toolbar.widget)
+
+	// Set up the ebiten game struct.
+	game := game{
+		ui: &ui,
+	}
+
+	// Event handling
+	//
+	// Example 1: Configure the "Help" button to display a message in console when it's pressed.
+	//
+	toolbar.helpButton.Configure(
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			println("The help button was pressed!")
+		}),
+	)
+
+	// Example 2: Configure the "Quit" menu entry to end the program when it's pressed.
+	toolbar.quitButton.Configure(
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			game.exit = true
+		}),
+	)
+
+	// Run the game.
+	if err := ebiten.RunGame(&game); err != nil {
+		log.Println(err)
+	}
+}
+
+type game struct {
+	ui   *ebitenui.UI
+	exit bool
+}
+
+// Update implements ebiten.Game.
+func (g *game) Update() error {
+	// Exit the game if the exit flag is set.
+	if g.exit {
+		return ebiten.Termination
+	}
+
+	// Update the UI
+	g.ui.Update()
+
+	return nil
+}
+
+// Draw implements ebiten.Game.
+func (g *game) Draw(screen *ebiten.Image) {
+	// Clear the screen with the color teal
+	screen.Fill(colornames.Teal)
+
+	// Draw the UI onto the screen
+	g.ui.Draw(screen)
+}
+
+// Layout implements ebiten.Game.
+func (g *game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+type resources struct {
+	font text.Face
+}
+
+func loadResources() (*resources, error) {
+	fnt, err := loadFont(16)
+	if err != nil {
+		return nil, err
+	}
+	return &resources{
+		font: fnt,
+	}, nil
+}
+
+func loadFont(size float64) (text.Face, error) {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return &text.GoTextFace{
+		Source: s,
+		Size:   size,
+	}, nil
+}
---
+++ _examples/toolbar/toolbar.go
@@ -0,0 +1,206 @@
+// toolbar.go
+//
+// Toolbar struct and related functions.
+//
+
+package main
+
+import (
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
+	"golang.org/x/image/colornames"
+	goimage "image"
+	"image/color"
+)
+
+// NOTE: It's not strictly necessary to store references to all the buttons in the toolbar struct, but this example does
+// so for completeness' sake. When you keep a reference to buttons in the struct, you can later configure them to respond
+// to certain events in your application, and keep your program's logic outside the toolbar.
+type toolbar struct {
+	widget      *widget.Container
+	fileMenu    *widget.Button
+	editMenu    *widget.Button
+	helpButton  *widget.Button
+	saveButton  *widget.Button
+	quitButton  *widget.Button
+	loadButton  *widget.Button
+	undoButton  *widget.Button
+	redoButton  *widget.Button
+	cutButton   *widget.Button
+	copyButton  *widget.Button
+	pasteButton *widget.Button
+}
+
+func newToolbar(ui *ebitenui.UI, res *resources) *toolbar {
+	// Create a root container for the toolbar.
+	root := widget.NewContainer(
+		// Use black background for the toolbar.
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.Black)),
+
+		// Toolbar components must be aligned horizontally.
+		widget.ContainerOpts.Layout(
+			widget.NewRowLayout(
+				widget.RowLayoutOpts.Direction(widget.DirectionHorizontal),
+			),
+		),
+
+		widget.ContainerOpts.WidgetOpts(
+			// Make the toolbar fill the whole horizontal space of the screen.
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{StretchHorizontal: true}),
+		),
+	)
+
+	//
+	// "File" menu
+	//
+	file := newToolbarButton(res, "File")
+	var (
+		save = newToolbarMenuEntry(res, "Save")
+		load = newToolbarMenuEntry(res, "Load")
+		quit = newToolbarMenuEntry(res, "Quit")
+	)
+	file.Configure(
+		// Make the toolbar entry open a menu with our "save" and "load" entries  when the user clicks it.
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			openToolbarMenu(args.Button.GetWidget(), ui, save, load, quit)
+		}),
+	)
+	root.AddChild(file)
+
+	//
+	// "Edit" menu
+	// This is the same thing as the "File" menu, just with more entries.
+	//
+	edit := newToolbarButton(res, "Edit")
+	var (
+		undo  = newToolbarMenuEntry(res, "Undo")
+		redo  = newToolbarMenuEntry(res, "Redo")
+		cut   = newToolbarMenuEntry(res, "Cut")
+		copy  = newToolbarMenuEntry(res, "Copy")
+		paste = newToolbarMenuEntry(res, "Paste")
+	)
+	edit.Configure(
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			openToolbarMenu(args.Button.GetWidget(), ui, undo, redo, cut, copy, paste)
+		}),
+	)
+	root.AddChild(edit)
+
+	//
+	// "Help" button
+	// Unlike the "File" and "Edit" menu, this is just a regular button on the toolbar - it does not open a menu.
+	// You can configure it to do something else when it's pressed, like opening a "Help" window.
+	//
+	help := newToolbarButton(res, "Help")
+	root.AddChild(help)
+
+	return &toolbar{
+		widget:      root,
+		fileMenu:    file,
+		editMenu:    edit,
+		helpButton:  help,
+		saveButton:  save,
+		loadButton:  load,
+		quitButton:  quit,
+		undoButton:  undo,
+		redoButton:  redo,
+		cutButton:   cut,
+		copyButton:  copy,
+		pasteButton: paste,
+	}
+}
+
+func newToolbarButton(res *resources, label string) *widget.Button {
+	// Create a button for the toolbar.
+	return widget.NewButton(
+		widget.ButtonOpts.Image(&widget.ButtonImage{
+			Idle:    image.NewNineSliceColor(color.Transparent),
+			Hover:   image.NewNineSliceColor(colornames.Darkgray),
+			Pressed: image.NewNineSliceColor(colornames.White),
+		}),
+		widget.ButtonOpts.Text(label, res.font, &widget.ButtonTextColor{
+			Idle:     color.White,
+			Disabled: colornames.Gray,
+			Hover:    color.White,
+			Pressed:  color.Black,
+		}),
+		widget.ButtonOpts.TextPadding(widget.Insets{
+			Top:    4,
+			Left:   4,
+			Right:  32,
+			Bottom: 4,
+		}),
+	)
+}
+
+func newToolbarMenuEntry(res *resources, label string) *widget.Button {
+	// Create a button for a menu entry.
+	return widget.NewButton(
+		widget.ButtonOpts.Image(&widget.ButtonImage{
+			Idle:    image.NewNineSliceColor(color.Transparent),
+			Hover:   image.NewNineSliceColor(colornames.Darkgray),
+			Pressed: image.NewNineSliceColor(colornames.White),
+		}),
+		widget.ButtonOpts.Text(label, res.font, &widget.ButtonTextColor{
+			Idle:     color.White,
+			Disabled: colornames.Gray,
+			Hover:    color.White,
+			Pressed:  color.Black,
+		}),
+		widget.ButtonOpts.TextPosition(widget.TextPositionStart, widget.TextPositionCenter),
+		widget.ButtonOpts.TextPadding(widget.Insets{Left: 16, Right: 64}),
+		widget.ButtonOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Stretch: true,
+			}),
+		),
+	)
+}
+
+func openToolbarMenu(opener *widget.Widget, ui *ebitenui.UI, entries ...*widget.Button) {
+	c := widget.NewContainer(
+		// Set the background to a translucent black.
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.RGBA{R: 0, G: 0, B: 0, A: 125})),
+
+		// Menu entries should be arranged vertically.
+		widget.ContainerOpts.Layout(
+			widget.NewRowLayout(
+				widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+				widget.RowLayoutOpts.Spacing(4),
+				widget.RowLayoutOpts.Padding(widget.Insets{Top: 1, Bottom: 1}),
+			),
+		),
+
+		// Set the minimum size for the menu.
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.MinSize(64, 0)),
+	)
+
+	for _, entry := range entries {
+		c.AddChild(entry)
+	}
+
+	w, h := c.PreferredSize()
+
+	window := widget.NewWindow(
+		// Set the menu to be a modal. This makes it block UI interactions to anything ese.
+		widget.WindowOpts.Modal(),
+		widget.WindowOpts.Contents(c),
+
+		// Close the menu if the user clicks outside of it.
+		widget.WindowOpts.CloseMode(widget.CLICK),
+
+		// Position the menu below the menu button that it belongs to.
+		widget.WindowOpts.Location(
+			goimage.Rect(
+				opener.Rect.Min.X,
+				opener.Rect.Min.Y+opener.Rect.Max.Y,
+				opener.Rect.Min.X+w,
+				opener.Rect.Min.Y+opener.Rect.Max.Y+opener.Rect.Min.Y+h,
+			),
+		),
+	)
+
+	// Immediately add the menu to the UI.
+	ui.AddWindow(window)
+}
