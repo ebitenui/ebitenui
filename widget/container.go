c36c874accce7fbff045af0d1c39987fd22b35d5
Jon Rafkind <jon@rafkind.com>
2025-06-22 21:30:22 -0700

    dont request a relayout when location is updated

--- widget/container.go
+++ widget/container.go
@@ -231,7 +231,6 @@ func (c *Container) PreferredSize() (int, int) {
 func (c *Container) SetLocation(rect img.Rectangle) {
 	c.init.Do()
 	c.widget.Rect = rect
-	c.RequestRelayout()
 }
 
 func (c *Container) Render(screen *ebiten.Image) {
